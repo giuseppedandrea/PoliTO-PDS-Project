7b4d7968d1d6d9322c818dd0be87a2068dd1f5e9
Giuseppe D'Andrea <s287584@studenti.polito.it>
2021-11-02 16:34:57 +0100

    Base version

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -32,6 +32,10 @@
 
 
 #include <cdefs.h> /* for __DEAD */
+#include "opt-syscalls.h"
+#include "opt-fork.h"
+#include "opt-file.h"
+
 struct trapframe; /* from <machine/trapframe.h> */
 
 /*
@@ -58,5 +62,22 @@ __DEAD void enter_new_process(int argc, userptr_t argv, userptr_t env,
 
 int sys_reboot(int code);
 int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
+#if OPT_SYSCALLS
+#if OPT_FILE
+struct openfile;
+void openfileIncrRefCount(struct openfile *of);
+int sys_open(userptr_t path, int openflags, mode_t mode, int *errp);
+int sys_close(int fd);
+#endif
+int sys_write(int fd, userptr_t buf_ptr, size_t size);
+int sys_read(int fd, userptr_t buf_ptr, size_t size);
+void sys__exit(int status);
+int sys_waitpid(pid_t pid, userptr_t statusp, int options);
+pid_t sys_getpid(void);
+#if OPT_FORK
+int sys_fork(struct trapframe *ctf, pid_t *retval);
+#endif
+
+#endif
 
 #endif /* _SYSCALL_H_ */
