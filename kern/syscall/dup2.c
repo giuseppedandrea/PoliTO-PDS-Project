193b9b3154914603b078c11e56cccdae01c48942
frsagco <francesco.aglieco@live.com>
2022-01-21 13:40:09 +0100

    System call lseek() implemented, dup2() partially
    lseek to be tested. For dup2 a new cirarray functionally is necessary

--- kern/syscall/dup2.c
+++ kern/syscall/dup2.c
@@ -15,9 +15,44 @@
 
 int sys_dup2(int oldfd, int newfd, int *errp)
 {
+    fcb fileToCpy, newFile;
+    int indTable;
 
+    if(oldfd<0 || oldfd>OPEN_MAX || newfd<0 || newfd>OPEN_MAX) {
+        *errp=EBADF;
+        return -1;
+    }
 
+    fileToCpy=sys_fileTable_get(proc_fileTable_get(curproc, oldfd));
+    if(fileToCpy==NULL) {
+        *errp=EBADF;
+        return -1;
+    }
 
+    newFile=sys_fileTable_get(proc_fileTable_get(curproc, newfd));
+    if(newFile!=NULL)
+    {
+        if(sys_close(newfd, errp)!=0) {
+            *errp=EBADF;
+            return -1;
+        }
+        freeFCB(newFile);
+    }
+
+    newFile=copyFCB(fileToCpy);
+    indTable=sys_fileTable_add(newFile);
+    if(indTable==0)
+        *errp = ENFILE;
+    else {
+        // qui serve una SET nel circulararray. IMplementare di corsa
+        fd=proc_fileTable_add(curproc, indTable); 
+        if(fd==0)
+        // no free slot in process open file table
+            *errp = EMFILE;
+        else
+            return fd;     
+    }
+       
 
 
 }
